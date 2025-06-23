5cdb2fd7a8cdb634d99bb895ac044d1302179364
AgroxOD <viktor.semenovych@agro-market.ua>
2025-06-23 20:59:00 +0300

    feat: validate auth routes

--- bot/src/controllers/authUser.js
+++ bot/src/controllers/authUser.js
@@ -1,17 +1,23 @@
 // Контроллеры регистрации и входа
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const { validationResult } = require('express-validator')
 const User = require('../models/User')
 const { jwtSecret } = require('../config')
 
-exports.register = async (req, res) => {
+function handle(req, res, next) {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+  return next()
+}
+exports.register = [handle, async (req, res) => {
   const { name, email, password } = req.body
   const hash = await bcrypt.hash(password, 12)
   const user = await User.create({ name, email, passwordHash: hash })
   res.status(201).json({ id: user._id, name: user.name, email: user.email })
-}
+}]
 
-exports.login = async (req, res) => {
+exports.login = [handle, async (req, res) => {
   const { email, password } = req.body
   const user = await User.findOne({ email })
   if (!user || !(await bcrypt.compare(password, user.passwordHash))) {
@@ -19,7 +25,7 @@ exports.login = async (req, res) => {
   }
   const token = jwt.sign({ id: user._id, role: user.role }, jwtSecret, { expiresIn: '8h' })
   res.json({ token })
-}
+}]
 
 exports.profile = async (req, res) => {
   const user = await User.findById(req.user.id).select('-passwordHash')

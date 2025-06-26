ec58f976fcf289eda5136911b344033dac7f053e
AgroxOD <viktor.semenovych@agro-market.ua>
2025-06-26 21:50:13 +0300

    Добавлены тесты статуса и ролей

---
+++ bot/tests/authRole.test.js
@@ -0,0 +1,20 @@
+// Тесты middleware checkRole: проверка доступа по ролям
+const express = require('express')
+const request = require('supertest')
+const checkRole = require('../src/middleware/checkRole')
+
+function appWithRole(role) {
+  const app = express()
+  app.get('/admin', (req, res, next) => { req.user = { role }; next() }, checkRole('admin'), (_req, res) => res.sendStatus(200))
+  return app
+}
+
+test('admin имеет доступ', async () => {
+  const res = await request(appWithRole('admin')).get('/admin')
+  expect(res.status).toBe(200)
+})
+
+test('пользователь получает 403', async () => {
+  const res = await request(appWithRole('user')).get('/admin')
+  expect(res.status).toBe(403)
+})

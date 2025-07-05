a5f6c78970c254f2fb07c065950e23aa6c5e8508
AgroxOD <viktor.semenovych@agro-market.ua>
2025-07-05 20:22:28 +0300

    feat: добавить React Quill и Telegram SDK

--- bot/web/src/main.tsx
+++ bot/web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import TelegramApp from "./TelegramApp";
 import "./index.css";
 
 const root = document.getElementById("root");
@@ -10,6 +10,6 @@ if (!root) {
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <TelegramApp />
   </React.StrictMode>,
 );

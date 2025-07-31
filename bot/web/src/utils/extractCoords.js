29c8371e12419078ebf8b1ee68c4d05efd034767
AgroxOD <viktor.semenovych@agro-market.ua>
2025-07-31 14:26:19 +0300

    Ревизия кода: вынесены функции Google Maps

--- bot/web/src/utils/extractCoords.js
+++ bot/web/src/utils/extractCoords.js
@@ -1,15 +1,6 @@
 // Извлекает координаты из ссылки Google Maps
-export default function extractCoords(url){
-  try{
-    let m=url.match(/@(-?\d+\.\d+),(-?\d+\.\d+)/);
-    if(!m){
-      m=url.match(/[?&]q=(-?\d+\.\d+),(-?\d+\.\d+)/);
-    }
-    if(m){
-      return {lat:parseFloat(m[1]),lng:parseFloat(m[2])};
-    }
-    return null;
-  }catch{
-    return null;
-  }
+import { extractCoords as sharedExtract } from '../../../shared/mapUtils.js';
+
+export default function extractCoords(url) {
+  return sharedExtract(url)
 }

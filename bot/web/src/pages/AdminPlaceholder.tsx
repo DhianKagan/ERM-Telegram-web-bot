02a64a458e238061ea9b719d8c0b9348084fa150
AgroxOD <viktor.semenovych@agro-market.ua>
2025-07-31 09:54:26 +0300

    Удалена неиспользуемая страница и скрипт, обновлена документация

--- bot/web/src/pages/AdminPlaceholder.tsx
+++
@@ -1,8 +0,0 @@
-// Страница-заглушка админки
-import React from 'react'
-
-export default function AdminPlaceholder() {
-  return (
-    <div className="p-4">Только Админ может увидеть содержание настоящей страницы админ</div>
-  )
-}

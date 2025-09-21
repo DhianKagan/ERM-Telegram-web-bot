c1ab7121fe7cbdfc7efc6106e4f3339ca40ce8a3
AgroxOD <viktor.semenovych@agro-market.ua>
2025-09-21 09:30:02 -0700

    fix: correct ckeditor lazy import resolution

--- apps/web/src/components/CKEditorPopup.tsx
+++ apps/web/src/components/CKEditorPopup.tsx
@@ -8,11 +8,9 @@ import { ensureWebpackNonce } from "../utils/ensureWebpackNonce";
 ensureWebpackNonce();
 
 const LazyCKEditor = React.lazy(async () => {
-  const reactEntry = "@ckeditor/ckeditor5-react";
-  const buildEntry = "@ckeditor/ckeditor5-build-classic";
   const [{ CKEditor }, { default: ClassicEditor }] = await Promise.all([
-    import(/* @vite-ignore */ reactEntry),
-    import(/* @vite-ignore */ buildEntry),
+    import("@ckeditor/ckeditor5-react"),
+    import("@ckeditor/ckeditor5-build-classic"),
   ]);
   type Props = React.ComponentProps<typeof CKEditor>;
   return {

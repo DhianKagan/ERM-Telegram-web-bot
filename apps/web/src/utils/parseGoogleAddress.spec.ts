ed6a9b883d823a63a0e09b1ae752ad77aa1d5fb8
Krishna Narayna <omkrishna.narayana@gmail.com>
2025-10-23 09:26:17 +0300

    Handle coordinate labels and driver names

--- apps/web/src/utils/parseGoogleAddress.spec.ts
+++ apps/web/src/utils/parseGoogleAddress.spec.ts
@@ -19,8 +19,13 @@ describe("parseGoogleAddress", () => {
     expect(parseGoogleAddress(invalid)).toBe(invalid);
   });
 
-  test("возвращает заглушку, когда название не найдено", () => {
-    const coordsUrl = "https://www.google.com/maps/@50.4501,30.5234,17z";
-    expect(parseGoogleAddress(coordsUrl)).toBe("Точка на карте");
+  test("возвращает координаты, когда в ссылке нет названия", () => {
+    const coordsUrl = "https://www.google.com/maps/@48.477836,30.70593,17z";
+    expect(parseGoogleAddress(coordsUrl)).toBe("48.477836, 30.705930");
+  });
+
+  test("возвращает заглушку, если координаты не найдены", () => {
+    const fallbackUrl = "https://maps.app.goo.gl/qYCv7Xdd8Q5xd2SFA";
+    expect(parseGoogleAddress(fallbackUrl)).toBe("Точка на карте");
   });
 });

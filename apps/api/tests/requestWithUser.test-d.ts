502c87b65635feb698e906b65cca8c1d0c2e9e83
Krishna Narayna <omkrishna.narayana@gmail.com>
2025-10-23 20:54:41 +0300

    Update type test for string user id

--- apps/api/tests/requestWithUser.test-d.ts
+++ apps/api/tests/requestWithUser.test-d.ts
@@ -3,6 +3,6 @@ import { expectType, expectError } from 'tsd';
 import { RequestWithUser } from '../src/types/request';
 
 const req = {} as RequestWithUser;
-expectType<number | undefined>(req.user?.id);
+expectType<string | number | undefined>(req.user?.id);
 expectType<number | undefined>(req.user?.telegram_id);
 expectError(req.user?.unknown);

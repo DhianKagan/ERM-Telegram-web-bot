009c8beda4b44743eafb45c9607fe2d265896105
Krishna Narayna <omkrishna.narayana@gmail.com>
2025-11-11 11:40:42 +0200

    chore(ci): add ts-node for ESLint TypeScript config

---
+++ patch_cjs/012-add-ts-node.cjs
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+// patch: 012-add-ts-node.cjs
+// purpose: добавить ts-node в devDependencies воркспейса
+const fs = require('fs');
+const pkgPath = 'package.json';
+const pkg = JSON.parse(fs.readFileSync(pkgPath,'utf8'));
+pkg.devDependencies ||= {};
+if (!pkg.devDependencies['ts-node']) {
+  pkg.devDependencies['ts-node'] = '^10.9.2';
+}
+fs.writeFileSync(pkgPath, JSON.stringify(pkg,null,2));
+console.log('[OK] devDep ts-node добавлен в корень');
